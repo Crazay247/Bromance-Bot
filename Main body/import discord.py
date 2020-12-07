ab8657114558c08f3fe7b5a20fc89a1e75aa436f
Crazay247 <72492357+Crazay247@users.noreply.github.com>
2020-12-07 17:09:33 +0545

    Update import discord.py

--- Main body/import discord.py
+++ Main body/import discord.py
@@ -2,4 +2,4 @@ import discord
 from discord.ext import commands 
 @client.event
 async def on ready 
-client.run('NzY4OTk2ODM3MjYwMDAxMzUw.X5ImHQ.4pqKTwYaE5c9HV-lvkyZS-xKBYE')   
+client.run('bottoken')   
